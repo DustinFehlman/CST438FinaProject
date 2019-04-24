489973ef52f4d1c6c05d472a76e07ec323bfdfb4
Dustin Fehlman <df1837@gmail.com>
2019-04-24 02:50:17 +0000

    Added logout and get current user endpoints.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,24 @@ class SessionsController < ApplicationController
     user = User.where(username: params[:username]).first
     if user && user.password == params[:password]
       log_in user
-      render json: build_res(nil, "User logged in.")
+      current_user
+      render json: build_res(nil, "User " + @current_user.username + " logged in.")
       return
     else
       render json: build_res("Unkown user or incorrect password.", nil)
       return
     end
   end
-
-  def destroy
+  
+  def getCurrentUser
+      render json: build_res(nil, "User: " + @current_user.username)
+      return
+  end
+  
+  def logout
+    log_out
+    render json: build_res(nil, "All users logged out.")
+    return
   end
 
   def build_res(error, data)
