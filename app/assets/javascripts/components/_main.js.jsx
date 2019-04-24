ab10d754d6b4884ad9233cd63b29c5c0eaef121a
Carlos Sanchez <ec2-user@ip-172-31-32-11.us-east-2.compute.internal>
2019-04-24 23:55:59 +0000

    started css

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -2,17 +2,16 @@
 const Main = (props) => {
     return(
         <div>
+        <div name="topbar">
+            <h1>Better Transportation Transparency (B.T.T)</h1>
 
-        <div className="pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">B.T.T.</a>
-            <ul className="pure-menu-list">
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Sign In</a></li>
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Log In</a></li>
-            </ul>
+            <nav>
+              <li><a className="active" href="#home">Better Transportation Transparency (B.T.T)</a></li>
+              <li><a href="#news">Sign Up</a></li>
+              <li><a href="#">Login</a></li>
+            </nav>
         </div>
 
-
-        <h1>Better Transportation Transparency (B.T.T)</h1>
         
         {/*<Signup/>*/}
         <Login/>
