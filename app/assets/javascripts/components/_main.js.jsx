8305e78f523f3bb1bc9549901ce0fe929ba79245
Carlos Sanchez <ec2-user@ip-172-31-32-11.us-east-2.compute.internal>
2019-04-25 00:02:01 +0000

    fixed nav css

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -2,15 +2,15 @@
 const Main = (props) => {
     return(
         <div>
-        <div name="topbar">
             <h1>Better Transportation Transparency (B.T.T)</h1>
 
-            <nav>
-              <li><a className="active" href="#home">Better Transportation Transparency (B.T.T)</a></li>
-              <li><a href="#news">Sign Up</a></li>
-              <li><a href="#">Login</a></li>
-            </nav>
-        </div>
+            <div className="pure-menu pure-menu-horizontal">
+            <a href="#" className="pure-menu-heading pure-menu-link">B.T.T.</a>
+            <ul className="pure-menu-list">
+                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Sign In</a></li>
+                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Log In</a></li>
+            </ul>
+            </div>
 
         
         {/*<Signup/>*/}
